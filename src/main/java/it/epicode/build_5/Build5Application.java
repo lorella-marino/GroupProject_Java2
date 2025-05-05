778e9bc0ccb6890724e94a12b9d196e202356864
lorella-marino <lorellamarino@gmail.com>
2025-05-05 10:33:49 +0200

    Initial commit

---
+++ src/main/java/it/epicode/build_5/Build5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.build_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Build5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Build5Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/build_5/Build5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.build_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Build5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
