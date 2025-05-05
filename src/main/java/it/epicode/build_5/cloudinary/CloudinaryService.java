428afadcb95b9d4f38af1a22d12717f28e6b6684
lorella-marino <lorellamarino@gmail.com>
2025-05-05 11:19:11 +0200

    add cloud

---
+++ src/main/java/it/epicode/build_5/cloudinary/CloudinaryService.java
@@ -0,0 +1,28 @@
+package it.epicode.build_5.cloudinary;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file) {
+        try {
+            Map result = cloudinary.uploader()
+                    .upload(file.getBytes(),  Cloudinary.asMap("folder", "FS1024", "public_id", file.getOriginalFilename()));
+
+            String url = result.get("secure_url").toString();
+
+            return url;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
